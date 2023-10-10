77a2c28ad9e1183a531fc01a90909ff6e94ccf98
Jason Spratt <sprattsj@gmail.com>
2023-10-10 14:29:29 -0400

    Task.java skeleton created

--- Task.java
+++ Task.java
@@ -1,3 +1,32 @@
-public abstract class Task {
-    
+import java.util.ArrayList;
+
+public abstract class Task extends Column{
+    private String taskName;
+    private String taskDesc;
+    private int taskPrio;
+    private Category taskCategory;
+    private ArrayList<Comment> taskThread;
+    private boolean inProgress;
+    private int taskPrivacy;
+    private String color;
+
+    public Task(String taskName, String taskDesc, int taskPrio, Category taskCategory, ArrayList<Comment> taskThread, boolean inProgress, int taskPrivacy, String color) {
+        this.taskName = taskName;
+        this.taskDesc = taskDesc;
+        this.taskPrio = taskPrio;
+        this.taskCategory = taskCategory;
+        this.taskThread = taskThread;
+        this.inProgress = inProgress;
+        this.taskPrivacy = taskPrivacy;
+        this.color = color;
+    }
+
+    public void markComplete() {
+        // TODO implement here
+    }
+
+    public boolean checkPrivacy(User userType) {
+        // TODO implement here
+        return false;
+    }
 }
