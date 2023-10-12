bbc97c921145544408df107da367c2b5c0e9c4e7
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-10-12 14:21:16 -0400

    fixed #19

--- Column.java
+++ Column.java
@@ -1,17 +1,17 @@
 public class Column {
     public ArrayList<Task> backlogList() {
-
+        return null;
     }
 
     public ArrayList<Task> todoList() {
-
+        return null;
     }
 
     public ArrayList<Task> inProgressList() {
-
+        return null;
     }
 
     public ArrayList<Task> CompletedList() {
-
+        return null;
     }
 }
