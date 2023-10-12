63cf51f3582de6b921cf9a0b31744c423696f580
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-10-12 14:05:14 -0400

    fixed #*

--- DataLoader.java
+++ DataLoader.java
@@ -1,3 +1,9 @@
-public class DataLoader {
+public class DataLoader extends DataConstraints {
     
+    public ArrayList<Projects> GetProjects() {
+
+    }
+    public ArrayList<User> GetUsers() {
+        
+    }
 }
