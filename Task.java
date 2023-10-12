3498f06540fd230848d76e869d55f6f35ec79d6a
Jason Spratt <sprattsj@gmail.com>
2023-10-12 14:29:19 -0400

    fixed #18

--- Task.java
+++ Task.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public abstract class Task extends Column{
+    // class variables
     private String taskName;
     private String taskDesc;
     private int taskPrio;
@@ -10,6 +11,17 @@ public abstract class Task extends Column{
     private int taskPrivacy;
     private String color;
 
+/**
+ * Constructor for Task
+ * @param taskName Name of the Task
+ * @param taskDesc Description of the Task
+ * @param taskPrio Priority of the Task
+ * @param taskCategory Category of the Task
+ * @param taskThread Thread of Comments for the Task
+ * @param inProgress Whether the Task is in progress or not
+ * @param taskPrivacy Privacy level of the Task
+ * @param color Color of the Task
+ */
     public Task(String taskName, String taskDesc, int taskPrio, Category taskCategory, ArrayList<Comment> taskThread, boolean inProgress, int taskPrivacy, String color) {
         this.taskName = taskName;
         this.taskDesc = taskDesc;
@@ -21,12 +33,22 @@ public abstract class Task extends Column{
         this.color = color;
     }
 
+/**
+ * Changes inProgress variable to false, representing that the Task is complete
+ */
     public void markComplete() {
-        // TODO implement here
+        inProgress = false;
     }
 
+/**
+ * Checks the privacy level of the User and compares it to the privacy level of the Task
+ * @param userType The User being checked upon to see if they have the correct privacy level
+ * @return boolean whether they have the correct privacy level or not
+ */
     public boolean checkPrivacy(User userType) {
-        // TODO implement here
-        return false;
+        if(userType.getPrivacy() == taskPrivacy)
+            return true;
+        else
+            return false;
     }
 }
