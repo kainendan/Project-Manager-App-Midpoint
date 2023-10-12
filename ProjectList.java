57850f0f83abe476c85acca3e7bbbdc7265c6ec0
Logan-Praylow <loganpraylow@gmail.com>
2023-10-12 14:13:10 -0400

    ProjectList code

--- ProjectList.java
+++ ProjectList.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class ProjectList {
-    
+    private static ProjectList projectList;
+    private ArrayList<Project> project;
+
+
+    public ProjectList(){
+        
+    }
+
+    public ProjectList getInstance(){
+       return projectList;
+    }
+
+    public ArrayList<Project> getProject(String Project){
+        return project;
+    }
+
 }
