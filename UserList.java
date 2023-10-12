eebf98285dd26b0a8dc1a37aa771ace4a9034899
Jason Spratt <sprattsj@gmail.com>
2023-10-12 14:04:06 -0400

    fixed #16

--- UserList.java
+++ UserList.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class UserList {
-    
+    private static UserList userList;
+    private ArrayList<User> users;
+
+    private UserList() {
+        //users = new ArrayList<User>();
+    }
+
+    public UserList getInstance() {
+        // TODO implement here  
+        return null;
+    }
+
+    public User getUser(String project) {
+        // TODO implement here
+        return null;
+    }
 }
