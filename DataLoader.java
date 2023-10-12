ba0c4b01bcc14047053f149198f789729bb40cfb
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-10-12 14:05:54 -0400

    Fiexd #8

--- DataLoader.java
+++ DataLoader.java
@@ -1,9 +1,10 @@
 public class DataLoader extends DataConstraints {
     
+    //stubs
     public ArrayList<Projects> GetProjects() {
 
     }
     public ArrayList<User> GetUsers() {
-        
+
     }
 }
