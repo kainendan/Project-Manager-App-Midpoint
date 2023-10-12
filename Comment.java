5d0a669bf469806738366114bf911fd13b4304fd
Jason Spratt <sprattsj@gmail.com>
2023-10-12 13:53:26 -0400

    fixed #15

---
+++ Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    private User commentAuthor;
+    private String commentText;
+    private String commentDate;
+
+    public Comment(User commentAuthor, String commentText, String commentDate) {
+        this.commentAuthor = commentAuthor;
+        this.commentText = commentText;
+        this.commentDate = commentDate;
+    }
+}
