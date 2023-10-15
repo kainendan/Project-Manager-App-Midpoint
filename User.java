9a3343c8aba354bbeef3d14ea1bf87a83ba21b34
Kainendan <bh67@email.sc.edu>
2023-10-15 14:09:44 -0400

    Fixed #20

--- User.java
+++ User.java
@@ -1,3 +1,40 @@
 public class User {
-    
+
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int privacyLevel;
+
+    public User(String username, String password, String email, String firstName, String lastName, int privacyLevel) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.privacyLevel = privacyLevel;
+    }
+
+    public boolean getPassword() {
+        return false;
+    }
+
+    public String getFirstName() {
+        return null;
+    }
+
+    public String getLastName() {
+        return null;
+    }
+
+    public String getEmail() {
+        return null;
+    }
+
+    public int getPrivacyLevel() {
+        return 0;
+    }
+
+
 }
