0489f10b901c362611d4e0a5df9385ef273e4422
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-10-17 13:42:44 -0400

    updated DataLoader GetUsers

--- DataLoader.java
+++ DataLoader.java
@@ -1,4 +1,10 @@
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.UUID;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class DataLoader extends DataConstants {
     public static void main(String[] args) {
@@ -8,7 +14,33 @@ public class DataLoader extends DataConstants {
         return null;
     }
     public ArrayList<User> GetUsers() {
-        return null;
-
+       ArrayList<User> users = new ArrayList<User>();
+		
+		try {
+			FileReader reader = new FileReader(USER_FILE_NAME);
+			JSONParser parser = new JSONParser();	
+			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < peopleJSON.size(); i++) {
+				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+				UUID id = UUID.fromString((String)personJSON.get(USER_ID));
+				String userName = (String)personJSON.get(USER_USER_NAME);
+				String firstName = (String)personJSON.get(USER_FIRST_NAME);
+				String lastName = (String)personJSON.get(USER_LAST_NAME);
+                String password = (String)personJSON.get(USER_PASSWORD);
+				int privacy = ((Long)personJSON.get(USER_PRIVACY)).intValue();
+				String email = (String)personJSON.get(USER_EMAIL);
+				
+                // add id when it can
+				users.add(new User(userName, password, firstName, lastName, email, privacy));
+			}
+			
+			return users;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
     }
 }
