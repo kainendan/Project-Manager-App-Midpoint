0b3ceaa476f4cdc9f95cd4babeb1ec25673bff03
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-10-17 14:07:26 -0400

    hard coding dataLoader

--- DataLoader.java
+++ DataLoader.java
@@ -2,9 +2,9 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.UUID;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
 
 public class DataLoader extends DataConstants {
     public static void main(String[] args) {
@@ -24,13 +24,13 @@ public class DataLoader extends DataConstants {
 				//UUID id = UUID.fromString((String)personJSON.get(USER_ID));
 				String projectName = "Temp_projectName";//(String)personJSON.get(PROJECT_NAME);
 				String projectdes = "description goes here";//(String)personJSON.get(PROJECT_DES);
-				User projectAuthor = user; //(String)personJSON.get(PROJECT_AUTHOR);
+				String projectAuthor = "temp_User"; //(String)personJSON.get(PROJECT_AUTHOR);
 				
                 // add id when it can
 				project.add(new Project(projectName, projectdes, projectAuthor));
 			//}
 			
-			return users;
+			return project;
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -43,12 +43,12 @@ public class DataLoader extends DataConstants {
        ArrayList<User> users = new ArrayList<User>();
 		
 		try {
-			FileReader reader = new FileReader(USER_FILE_NAME);
-			JSONParser parser = new JSONParser();	
-			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			//FileReader reader = new FileReader(USER_FILE_NAME);
+			//JSONParser parser = new JSONParser();	
+			//JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
 			
-			for(int i=0; i < peopleJSON.size(); i++) {
-				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+			//for(int i=0; i < peopleJSON.size(); i++) {
+				//JSONObject personJSON = (JSONObject)peopleJSON.get(i);
 				//UUID id = UUID.fromString((String)personJSON.get(USER_ID));
 				String userName = "Temp_userName";//(String)personJSON.get(USER_USER_NAME);
 				String firstName = "sam";//(String)personJSON.get(USER_FIRST_NAME);
@@ -59,7 +59,7 @@ public class DataLoader extends DataConstants {
 				
                 // add id when it can
 				users.add(new User(userName, password, firstName, lastName, email, privacy));
-			}
+			//}
 			
 			return users;
 			
