3a9b5a335131d7ae73e7a27e14cc96bde4e7e041
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-10-18 23:38:25 -0400

    uncomment GetUsers json

--- DataLoader.java
+++ DataLoader.java
@@ -14,6 +14,7 @@ public class DataLoader extends DataConstants {
     }
     public static ArrayList<Project> GetProjects() {
         ArrayList<Project> project = new ArrayList<Project>();
+        //FileReader reader = new FileReader(PROJECT_FILE_NAME)
         //ArrayList<User> user = new ArrayList<User>();
 		
 		try {
@@ -45,23 +46,23 @@ public class DataLoader extends DataConstants {
        ArrayList<User> users = new ArrayList<User>();
 		
 		try {
-			//FileReader reader = new FileReader(USER_FILE_NAME);
-			//JSONParser parser = new JSONParser();	
-			//JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			FileReader reader = new FileReader(USER_FILE_NAME);
+			JSONParser parser = new JSONParser();	
+			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
 			
-			//for(int i=0; i < peopleJSON.size(); i++) {
-				//JSONObject personJSON = (JSONObject)peopleJSON.get(i);
-				//UUID id = UUID.fromString((String)personJSON.get(USER_ID));
-				String userName = "Temp_userName";//(String)personJSON.get(USER_USER_NAME);
-				String firstName = "sam";//(String)personJSON.get(USER_FIRST_NAME);
-				String lastName = "howards"; //(String)personJSON.get(USER_LAST_NAME);
-                String password = "password";//(String)personJSON.get(USER_PASSWORD);
-				int privacy = 0;//((Long)personJSON.get(USER_PRIVACY)).intValue();
-				String email = "sam@email.com";//(String)personJSON.get(USER_EMAIL);
+			for(int i=0; i < peopleJSON.size(); i++) {
+				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+				UUID id = UUID.fromString((String)personJSON.get(USER_ID));
+				String userName = (String)personJSON.get(USER_USER_NAME);
+				String firstName = (String)personJSON.get(USER_FIRST_NAME);
+				String lastName = (String)personJSON.get(USER_LAST_NAME);
+                String password = (String)personJSON.get(USER_PASSWORD);
+				int privacy = users.getPrivacy();
+				String email = (String)personJSON.get(USER_EMAIL);
 				
                 // add id when it can
 				users.add(new User(userName, password, firstName, lastName, email, privacy));
-			//}
+			}
 			
 			return users;
 			
