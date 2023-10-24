d7d1d0e79e60540220b0fd87e6ad2a0971a5a4a6
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-10-24 13:19:54 -0400

    Adding javadoc

--- UserList.java
+++ UserList.java
@@ -1,14 +1,22 @@
+// RJ Allen
 import java.util.ArrayList;
 
 public class UserList {
     private static UserList userList = null;
     private static ArrayList<User> users = new ArrayList<>();
 
+    /**
+     * Constructor for the list of users
+     */
     private UserList() {
         users = new ArrayList<User>();
         users = DataLoader.GetUsers();
     }
 
+
+    /**
+     * @return the instance of the userlist
+     */
     public static UserList getInstance() {
         if(userList == null) {
             System.out.println("Creating a new user account: ");
@@ -17,14 +25,32 @@ public class UserList {
         return userList;
     }
 
+    /**
+     * @return the user in the userList
+     */
     public ArrayList<User> getUserList() {
         return users;
     }
 
+    /**
+     * Add user to the userList
+     * @param userName
+     * @param password
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param privacyLevel
+     */
     public static void addUser(String userName, String password, String firstName, String lastName, String email, int privacyLevel) {
         users.add(new User(userName, password, firstName, lastName, email, privacyLevel));
     }
 
+    /**
+     * Check if user is valid
+     * @param userName
+     * @param password
+     * @return true if the user match the parameters
+     */
     public static boolean isValidLogin(String userName, String password) {
         for(User user : users) {
             if(user.getUserName().equals(userName) && user.getPassword().equals(password)) {
@@ -34,6 +60,9 @@ public class UserList {
         return false;
     }
 
+    /**
+     * logs out user and writers user info
+     */
     public void logout() {
         DataWriter.saveUsers();
     }
