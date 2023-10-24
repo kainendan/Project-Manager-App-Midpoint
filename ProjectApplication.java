0da98d339eaae14e33c4da4f5047da1bf35007a8
nicka <arboscen@email.sc.edu>
2023-10-24 13:41:35 -0400

    wrote methods for respective classes

--- ProjectApplication.java
+++ ProjectApplication.java
@@ -1,3 +1,130 @@
+//Nick Arboscello
+import java.util.ArrayList;
+
+/**
+ * The ProjectApplication class represents the main application for managing projects, users, and related functionality.
+ */
 public class ProjectApplication {
-    
+    private ProjectList projectList;
+    private UserList userList;
+    private User user;
+  
+    /**
+     * Constructs a new ProjectApplication object.
+     */
+    public ProjectApplication() {
+        projectList = new ProjectList(); // Initialize projectList
+        userList = new UserList(); // Initialize userList
+        user = null; // Initialize user
+    }
+
+    /**
+     * Attempts to log in a user with the given username and password.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @return The User object if login is successful, or null if login fails.
+     */
+    public User login(String username, String password) {
+        // Implementation details for login.
+        // Replace with actual logic.
+        return null; // Placeholder return value.
+    }
+
+    /**
+     * Creates a new user account.
+     *
+     * @return The newly created User object.
+     */
+    public User signUp() {
+        // Implementation details for user signup.
+        // Replace with actual logic.
+        return new User(); // Placeholder return value.
+    }
+
+    /**
+     * Retrieves a list of all projects.
+     *
+     * @return A list of all projects.
+     */
+    public ArrayList<Project> getAllProjects() {
+        // Implementation details for getting all projects.
+        // Replace with actual logic.
+        return new ArrayList<>(); // Placeholder return value.
+    }
+
+    /**
+     * Finds and returns a list of matching projects based on some criteria.
+     *
+     * @return A list of matching projects.
+     */
+    public ArrayList<Project> findProject() {
+        // Implementation details for finding projects.
+        // Replace with actual logic.
+        return new ArrayList<>(); // Placeholder return value.
+    }
+
+    /**
+     * Creates a new user account.
+     *
+     * @param firstName The first name of the user.
+     * @param lastName  The last name of the user.
+     * @return The newly created User object.
+     */
+    public User createAccount(String firstName, String lastName) {
+        // Implementation details for creating a user account.
+        // Replace with actual logic.
+        return new User(); // Placeholder return value.
+    }
+
+    /**
+     * Creates a new project.
+     *
+     * @param projectName The name of the project.
+     * @param projectDesc The description of the project.
+     * @return True if the project is created successfully, false otherwise.
+     */
+    public boolean createProject(String projectName, String projectDesc) {
+        // Implementation details for creating a project.
+        // Replace with actual logic.
+        return false; // Placeholder return value.
+    }
+
+    /**
+     * Creates a new task for a project.
+     *
+     * @param taskName  The name of the task.
+     * @param taskDesc  The description of the task.
+     * @return True if the task is created successfully, false otherwise.
+     */
+    public boolean createTask(String taskName, String taskDesc) {
+        // Implementation details for creating a task.
+        // Replace with actual logic.
+        return false; // Placeholder return value.
+    }
+
+    /**
+     * Creates a new comment for a task.
+     *
+     * @param commentAuth The user authoring the comment.
+     * @param commentText The text of the comment.
+     * @return True if the comment is created successfully, false otherwise.
+     */
+    public boolean createComment(User commentAuth, String commentText) {
+        // Implementation details for creating a comment.
+        // Replace with actual logic.
+        return false; // Placeholder return value.
+    }
+
+    /**
+     * Edits a task with the given taskId.
+     *
+     * @param taskId The ID of the task to be edited.
+     * @param task   The updated task information.
+     */
+    public void editTask(int taskId, ArrayList<String> task) {
+        // Implementation details for editing a task.
+        // Replace with actual logic.
+    }
 }
+
--- RegisteredUser.java
+++ RegisteredUser.java
@@ -1,3 +1,46 @@
+// Nick Arboscello
+/**
+ * The RegisteredUser class represents a registered user in the system.
+ */
 public class RegisteredUser {
-    
+    private String userName;    // The username of the registered user.
+    private String password;    // The password of the registered user.
+    private String firstName;   // The first name of the registered user.
+    private String lastName;    // The last name of the registered user.
+    private String email;       // The email address of the registered user.
+    private int privacyLevel;   // The privacy level of the registered user.
+
+    /**
+     * Constructs a new RegisteredUser object.
+     */
+    public RegisteredUser() {
+        // Constructor for RegisteredUser.
+        // Initialize member variables as needed.
+    }
+
+    /**
+     * Adds a main project for the registered user.
+     */
+    public void addMainProject() {
+        // Implementation details for adding a main project.
+        // Replace with actual logic.
+    }
+
+    /**
+     * Adds a project manager role for the registered user.
+     */
+    public void addProjectManager() {
+        // Implementation details for adding a project manager role.
+        // Replace with actual logic.
+    }
+
+    /**
+     * Gets the username of the registered user.
+     *
+     * @return The username of the registered user.
+     */
+    public String getUsername() {
+        return userName;
+    }
 }
+
