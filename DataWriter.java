7308bcd184e3238242db8edbb094e63b68a055a0
Kainendan <bh67@email.sc.edu>
2023-10-31 13:14:28 -0400

    Added Project DataWriter

--- DataWriter.java
+++ DataWriter.java
@@ -7,7 +7,6 @@ import org.json.simple.JSONObject;
 
 public class DataWriter extends DataConstants {
 	
-	
 	public static void saveUsers() {
 		UserList users = UserList.getInstance();
 		ArrayList<User> userList = users.getUserList();
@@ -39,6 +38,38 @@ public class DataWriter extends DataConstants {
 		userDetails.put(USER_EMAIL, user.getEmail());
         
         return userDetails;
+	}
+
+	public static void saveProjects() {
+		ProjectList projects = ProjectList.getInstance();
+		ArrayList<Project> projectList = projects.getProjectList();
+		JSONArray jsonProjects = new JSONArray();
+		
+		//creating all the json objects
+		for(int i=0; i< projectList.size(); i++) {
+			jsonProjects.add(getProjectJSON(projectList.get(i)));
+		}
+		
+		//Write JSON file
+		try (FileWriter file = new FileWriter(PROJECT_FILE_NAME)) {
+ 
+			file.write(jsonProjects.toJSONString());
+			file.flush();
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getProjectJSON(Project project) {
+		JSONObject projectDetails = new JSONObject();
+		projectDetails.put(PROJECT_ID, project.getId().toString());
+		projectDetails.put(PROJECT_NAME, project.getProjectName());
+		projectDetails.put(PROJECT_DESCRIPTION, project.getProjectDesc());
+		projectDetails.put(PROJECT_AUTHOR, project.getProjectAuthor());
+		//projectDetails.put(PROJECT_TASK, project.getTasks());
+		
+		return projectDetails;
 	}
 }
 
--- ProjectManagerUI.java
+++ ProjectManagerUI.java
@@ -74,11 +74,6 @@ public class ProjectManagerUI {
                         break;
                 }
             }
-
         }
-    }
-
-    
-
-    
+    } 
 }
