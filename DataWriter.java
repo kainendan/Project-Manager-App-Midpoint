084bde5c8abd733574a96940e9869e288b27571c
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-11-01 11:13:34 -0400

    editing datawriter

--- DataWriter.java
+++ DataWriter.java
@@ -100,7 +100,7 @@ public class DataWriter extends DataConstants {
 		taskDetails.put(TASK_NAME, task.getTaskName());
 		taskDetails.put(TASK_DESC, task.getTaskDes());
 		taskDetails.put(TASK_PRIO, task.getTaskPrio());
-		taskDetails.put(TASK_CATE, task.getCategory());
+		taskDetails.put(TASK_CATE, task.getCategory().toString());
 		taskDetails.put(TASK_THREAD, task.getComments());
 
 		taskDetails.put(TASK_INPROGRESS, task.getInProgress());
