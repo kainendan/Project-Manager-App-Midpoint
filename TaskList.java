d0990002eb3a40cbeab76c1a1af7058d80bf9c85
Gamerbro3333 <robertallen@my.tridenttech.edu>
2023-11-03 17:05:01 -0400

    Adding @Test

--- TaskList.java
+++ TaskList.java
@@ -1,13 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 // RJ Allen
 // This is to possible help with adding task for UI and dataWriter
 import java.util.ArrayList;
+
 public class TaskList {
     private static TaskList taskList = null;
     private static ArrayList<Task> tasks = new ArrayList<>();
 
+    
      /**
      * Constructor for the list of tasks
      */
+    
     public TaskList() {
         tasks = new ArrayList<Task>();
         tasks = DataLoader.GetTask();
@@ -16,6 +22,7 @@ public class TaskList {
      /**
      * @return the instance of the tasklist
      */
+    @Test
     public static TaskList getInstance() {
         if(taskList == null) {
             taskList = new TaskList();
@@ -26,6 +33,7 @@ public class TaskList {
      /**
      * @return the task in the tasklist
      */
+    @Test
     public ArrayList<Task> geTasksList() {
         return tasks;
     }
@@ -41,6 +49,7 @@ public class TaskList {
      * @param taskPrivacy
      * @param color
      */
+    @Test
     public static void addTask(String taskName, String taskDesc, int taskPrio, Category taskCategory, ArrayList<Comment> taskThread, boolean inProgress, int taskPrivacy, String color) {
         tasks.add(new Task(taskName, taskDesc, taskPrio, taskCategory, taskThread, inProgress, taskPrivacy, color));
     }
