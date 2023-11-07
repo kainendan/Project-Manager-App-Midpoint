1fce0766c753352f51de8ce0314b8d85d75a8292
Logan-Praylow <loganpraylow@gmail.com>
2023-11-07 14:11:10 -0500

    need to fix variables after exam

--- DataLoaderTest.java
+++ DataLoaderTest.java
@@ -1,8 +1,51 @@
 //@author Logan Praylow
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.UUID;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 public class DataLoaderTest {
     
+    private UserList users = UserList.getInstance();
+	private ArrayList<User> userList = users.getUserList();
+	
+	@BeforeEach
+	public void setup() {
+		UserList.clear();
+		UserList.addUser(new User(UUID.randomUUID(),"jb","1234","Josh","bailey","jb@gmail.com",1));
+		UserList.addUser(new User(UUID.randomUUID(),"jb","1234","Josh","bailey","jb@gmail.com",1));
+		DataWriter.saveUsers();
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		UserList.getInstance().getUserList().clear();
+		DataWriter.saveUsers();
+	}
+	
+	
+	@Test
+	void testGetUsersSize() {
+		userList = DataLoader.getUserList();
+		assertEquals(2, userList.size());
+	}
+
+	@Test
+	void testGetUsersSizeZero() {
+		UserList.getInstance().getUserList().clear();
+		DataWriter.saveUsers();
+		assertEquals(0, userList.size());
+	}
+	
+	@Test
+	void testGetUserFirstUserName() {
+		userList = DataLoader.getUserList();
+		assertEquals("asmith", userList.get(0).getUserName());
+	}
+
+
+
 }
